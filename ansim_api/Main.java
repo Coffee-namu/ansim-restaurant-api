8f0fa1345fe90f1e3ccecd6ce47a7be4d87f9471
sshrik <chj8781942naver.com>
2020-11-22 16:32:52 +0900

    Combine JAVA File.

--- ansim_api/Main.java
+++ ansim_api/Main.java
@@ -1,11 +1,13 @@
 package ansim_api;
 
+import java.util.Vector;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 
 class Main {
     public static void main(String[] args) {
+        // @Author : CHJ
         SeoulAnsimUpsoInfo seoul = new SeoulAnsimUpsoInfo();
         try {
             JSONArray retValJS = seoul.requestDataArr();
@@ -14,7 +16,28 @@ class Main {
             }
         }
         catch(Exception e){
-            System.out.println("ERR!");
+            e.printStackTrace(); 
+        }
+
+        // @Author : LJS
+        Parser p1 = new Parser();
+        Parser p2 = new Parser();
+        String fname1 = "경기도안심식당정보.csv";
+        String fname2 = "농림축산부안심식당정보.csv";
+        
+        try {
+            p1.CSVParser(fname1);
+            Vector<JSONObject> jobjs1 = p1.JsonParser();
+            for(int i=0;i<jobjs1.size();i++)
+            	System.out.println(jobjs1.get(i));
+            
+            p2.CSVParser(fname2);
+            Vector<JSONObject> jobjs2 = p2.JsonParser();
+            for(int i=0;i<jobjs2.size();i++)
+            	System.out.println(jobjs2.get(i));
+        }
+        catch (Exception e) { 
+            e.printStackTrace(); 
         }
     }
 }
---
+++ ansim_api/Parser.java
@@ -0,0 +1,57 @@
+package ansim_api;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+
+import org.json.simple.JSONObject;
+
+public class Parser {
+    private Vector<String> jsonData = new Vector<String>();
+
+	public void CSVParser(String filename) {
+        try{
+            FileReader input = new FileReader(filename);
+            BufferedReader br = new BufferedReader(input); 
+
+            String line = "";
+
+            while ((line = br.readLine()) != null)
+                this.jsonData.add(line);
+          
+            br.close();
+        }
+        catch (FileNotFoundException e) { 
+            e.printStackTrace(); 
+        }
+        catch (IOException e) { 
+            e.printStackTrace(); 
+        }
+	}
+
+    public Vector<JSONObject> JsonParser() {
+    	Vector<JSONObject> jobjs = new Vector<JSONObject>();
+        String[] category = this.jsonData.get(0).split(",");
+        int catenum = category.length;
+        int objnum = this.jsonData.size();
+
+        for(int j=1; j<objnum; j++){
+            String[] tmp = new String[catenum];
+            tmp = this.jsonData.get(j).split(",",-1);
+            JSONObject jobj = new JSONObject();
+
+            for(int k=0; k<catenum; k++) {
+            	if(tmp[k]=="" || tmp[k]==" ")
+            		jobj.put(category[k],null);
+            	else
+            		jobj.put(category[k],tmp[k]);
+            }
+            	
+            jobjs.add(jobj);
+        }
+        
+        return jobjs;
+    }
+}
